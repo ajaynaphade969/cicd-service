4ab9e06a38ffce6e30931ff2923217d39919578a
AJ <54471841+ajaynaphade969@users.noreply.github.com>
2022-01-04 18:43:31 +0530

    Update server.js
    Removed <h2> Added Service Account for Cloud Build </h2>

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Deployed Version is v2.0. Cloud Build in action.......</h1> <br/> <h2> Added Service Account for Cloud Build </h2>");
+  response.end("<h1> Deployed Version is v2.0. Cloud Build in action.......</h1> <br/> ");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
