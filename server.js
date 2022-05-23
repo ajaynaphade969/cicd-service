a59100f4956a1e805d28f66f9843b095bb42741c
AJ <54471841+ajaynaphade969@users.noreply.github.com>
2022-05-23 17:11:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("<h1> Hello ANZ </h1> <br/>
-               <h2>Cloud Build in action.......</h2> <br/> 
+  response.end("<h1> Hello ANZ </h1> <br/><h2> Cloud Build in action.......</h2> <br/> 
                <b> Remember to append -sha256-1 to the container(deployment) name like hello-cloudbuild-demo-app-sha256-1 </b>
-  <br/> updated on 05-01-2022");
+  <br/> updated on 23-05-2022");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
